915360e1bc94c8f5c6ff68483fe38bd95604e282
Akshad Ap <akshadapastambh17@gmail.com>
2025-12-04 15:18:28 +0530

    Update backend message to Akshad

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 
 // Simple API endpoint
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from Backend! Akshad" });
+  res.json({ message: "Hello from Backend! Akshad11/17" });
 });
 
 
